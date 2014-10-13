d4f01c959c35b13d1a17d0483120eb7472832bed
Josep Oriol Vilarrubi Barri <josep.vilarrubibarri@bsc.es>
2014-10-13 20:08:51 +0200

    minor changes

--- src/master/Master.java
+++ src/master/Master.java
@@ -1,20 +1,27 @@
 package master;
 
-import java.awt.BorderLayout;
+import java.awt.CheckboxGroup;
 import java.awt.Dimension;
-import java.awt.Frame;
 import java.awt.GraphicsDevice;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.DefaultListModel;
 import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
+import javax.swing.JTable;
 
 public class Master extends JFrame {
     private final String defaultLayout = "layoutES.ser";
@@ -32,31 +39,28 @@ public class Master extends JFrame {
     private Teclat teclat;
     private Trackpad trackpad;
     private TauletaGrafica tgrafica;
+    
+    private JPanel pOpcions;
+    private JButton exitButton;
+    private List<JCheckBox> llistaClients;
 
     public Master() throws IOException {
         super("SER");
-//        ArrayList<Double> tamanys = new ArrayList<>();
-//        llista = new ArrayList();
-//        scanSock = new DatagramSocket();
-//        scanBuff = new byte[client.Client.BUFF_LEN];
-//        scanPack = new DatagramPacket(scanBuff, client.Client.BUFF_LEN);
-//        model = new DefaultListModel<>();
-//        jList1.setModel(model);
-//        jSlider1.setMaximum(100);
-
         GraphicsDevice gd = getGraphicsConfiguration().getDevice();
         setUndecorated(true);
-//        setResizable(false);
-        if (gd.isFullScreenSupported()) {
-            gd.setFullScreenWindow(this);
-        } else {
+        setResizable(false);
+//        if (gd.isFullScreenSupported()) {
+//            System.err.println("fullscreen");
+//            gd.setFullScreenWindow(this);            
+//        } else {
+//            System.err.println("NO fullscreen");
             setPreferredSize(getGraphicsConfiguration().getBounds().getSize());
             setExtendedState(JFrame.MAXIMIZED_BOTH);
             boolean success = (getExtendedState() == JFrame.MAXIMIZED_BOTH);
             if (!success) {
-                setExtendedState(Frame.MAXIMIZED_BOTH);
+                System.err.println("Could not maximize the window");
             }
-        }
+//        }
         setVisible(true);
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         tabs = new JTabbedPane();
@@ -74,10 +78,12 @@ public class Master extends JFrame {
         
         
         Dimension screenSize = getSize();
-        screenSize.height-=30;
+        screenSize.height-=42;
 
         //INIT TABS
         tabs.setFocusable(false);
+        float f = 25;
+        tabs.setFont(tabs.getFont().deriveFont(f));
 
         teclat = new Teclat(defaultLayout, screenSize);
         trackpad = new Trackpad(screenSize);
@@ -93,6 +99,56 @@ public class Master extends JFrame {
         teclat.setFocusable(true);
         teclat.requestFocusInWindow();
         teclat.setFocusTraversalKeysEnabled(false);
+        
+        //Pantalla opcions
+        pOpcions = new JPanel();
+        GroupLayout opcionsLayout = new GroupLayout(pOpcions);
+        pOpcions.setLayout(opcionsLayout);
+        
+        //BOTO exit
+        exitButton = new JButton("Exit");
+        exitButton.setFont(exitButton.getFont().deriveFont(f));
+        exitButton.setSize(screenSize.width/10,screenSize.height/10 );
+        pOpcions.add(exitButton);
+        exitButton.setLocation(screenSize.width - screenSize.width/10,0);
+        exitButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                exitSER(null);
+            }
+        });
+        
+        //Llista clients
+        
+        model = new DefaultListModel<>();
+        model.addElement("alfa");
+        model.addElement("beta");
+        llistaClients = new ArrayList<>();
+        llistaClients.add(new JCheckBox("alfa", false));
+        llistaClients.add(new JCheckBox("beta", true));
+        llistaClients.add(new JCheckBox("omega", false));
+        int ypos=30;
+        for (JCheckBox box : llistaClients) {
+            box.setSize(screenSize.width/5,30);
+            box.setFont(box.getFont().deriveFont(f));
+            box.setLocation(0, ypos);
+            ypos+=30;
+            
+            pOpcions.add(box);
+        }
+        
+        JLabel titol= new JLabel("Client list");
+        titol.setFont(titol.getFont().deriveFont(f));
+        titol.setSize(screenSize.width/5,25);
+        titol.setLocation(0, 0);
+        pOpcions.add(titol);
+        pOpcions.setName("Opcions");
+        
+        //Fi opcions
+        tabs.add(pOpcions);
+        tabs.setSelectedIndex(3);//temporal per a veure millor la pantalla opcions
+        
         pack();
 
     }
@@ -110,7 +166,7 @@ public class Master extends JFrame {
                 }
             }
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Finestra.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Master.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
@@ -122,11 +178,23 @@ public class Master extends JFrame {
                     Master w = new Master();
                     w.setVisible(true);
                 } catch (IOException ex) {
-                    Logger.getLogger(Finestra.class.getName()).log(Level.SEVERE, null, ex);
+                    Logger.getLogger(Master.class.getName()).log(Level.SEVERE, null, ex);
                 }
 
             }
         });
+    }
+    
+    private void exitSER(String Error){
+        if(Error != null && !Error.equalsIgnoreCase("Normal Exit")){
+            System.err.print("Exiting with error: ");
+            System.err.println(Error);
+            System.exit(1);
+        }
+        else{
+            System.exit(0);
+        }
+        
     }
 
 }
