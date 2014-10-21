4d13873f718f67dfdcfd03611aabdb511eeb6aca
Josep Oriol Vilarrubi Barri <josep.vilarrubibarri@bsc.es>
2014-10-21 17:50:24 +0200

    minor changes

--- src/master/Emiter.java
+++ src/master/Emiter.java
@@ -5,63 +5,92 @@ import java.io.PrintStream;
 import java.net.Socket;
 import java.net.SocketAddress;
 
-public class Emiter{
-    
-    public static final int KEYBOARD_TYPE = 0; 
-    public static final int MOUSE_TYPE = 1; 
-    public static final int TABLET_TYPE = 2; 
-    public static final int PRESSED = 0; 
-    public static final int RELEASED = 1; 
-    public static final int BUTTON_PRESSED = 0; 
-    public static final int BUTTON_RELEASED = 1; 
-    
-    private Socket sock;
+public class Emiter {
+
+    public static final int KEYBOARD_TYPE = 0;
+    public static final int MOUSE_TYPE = 1;
+    public static final int TABLET_TYPE = 2;
+    public static final int PRESSED = 0;
+    public static final int RELEASED = 1;
+    public static final int BUTTON_PRESSED = 0;
+    public static final int BUTTON_RELEASED = 1;
+
     private final String name;
+    private Socket sock;
     private PrintStream out;
-    public Emiter(SocketAddress addr,String name) throws IOException {
+    private SocketAddress addr;
+    private boolean connected = false;
+
+    public Emiter(SocketAddress addr, String name) throws IOException {
+        this.addr = addr;
         this.name = name;
         this.sock = new Socket();
-        sock.connect(addr);
-        out = new PrintStream(sock.getOutputStream());
     }
 
-    /**This function send a message ordering to do some action with the "virtual"
-     * keyboard or mouse.
-     * 
-     * <p>
-     * The different types  and subtypes of messages are the following ones:
-     * KEYBOARD_TYPE: it is a message from the keyboard, their subtypes are:
-     * PRESSED
-     * RELEASED
-     * The message it is always a representation of the key pressed/released
-     * MOUSE_TYPE: it is a message from the mouse, their subtypes are:
-     * BUTTON_PRESSED
-     * BUTTON_RELEASED
-     * The message is the number of the button that has been pressed/released
-     * MOVE
-     * The message is the x & y position in the range 0-1 in double precision: Example 0.23322,0.85686
-     * WHEEL
-     * The message is the movement of the mouse wheel
-     * TABLET_TYPE: it is a message form the graphical tablet, their subtypes are
-     * the same as in the MOUSE
-     * 
-     * @param type  this parameter defines which message types this is
-     * @param subtype  this parameter defines which subtype of message this is
-     * @param Message  the char to send to the Receiver
-     */
-    public void write(int type,int subtype, String Message){
-        out.println(type + ":" + subtype +":"+ Message + "\n");;
-        out.flush();
+    public boolean isConnected() {
+        return connected;
     }
-    
-    public void stop() throws IOException{
-        out.close();
-        sock.close();
+
+    public boolean connect() {
+        System.out.println("soc "+name+" i mestic connectant");
+        connected = true;
+        return true;
+//        if (!connected) {
+//            boolean success = true;
+//            try {
+//                sock.connect(addr);
+//                out = new PrintStream(sock.getOutputStream());
+//            } catch (IOException ex) {
+//                Logger.getLogger(Emiter.class.getName()).log(Level.SEVERE, null, ex);
+//                success = false;
+//            }
+//            connected = success;
+//            return success;
+//        } else {
+//            return false;
+//        }
     }
-    
-    public String getName(){
+
+    public String getName() {
         return name;
-
     }
-    
+
+    public boolean amI(String name) {
+        return this.name.equals(name);
+    }
+
+    /**
+     * This function send a message ordering to do some action with the
+     * "virtual" keyboard or mouse.
+     *
+     * <p>
+     * The different types and subtypes of messages are the following ones:
+     * KEYBOARD_TYPE: it is a message from the keyboard, their subtypes are:
+     * PRESSED RELEASED The message it is always a representation of the key
+     * pressed/released MOUSE_TYPE: it is a message from the mouse, their
+     * subtypes are: BUTTON_PRESSED BUTTON_RELEASED The message is the number of
+     * the button that has been pressed/released MOVE The message is the x & y
+     * position in the range 0-1 in double precision: Example 0.23322,0.85686
+     * WHEEL The message is the movement of the mouse wheel TABLET_TYPE: it is a
+     * message form the graphical tablet, their subtypes are the same as in the
+     * MOUSE
+     *
+     * @param type this parameter defines which message types this is
+     * @param subtype this parameter defines which subtype of message this is
+     * @param Message the char to send to the Receiver
+     */
+    public void write(int type, int subtype, String Message) {
+        if (connected) {
+            out.println(type + ":" + subtype + ":" + Message + "\n");
+            out.flush();
+        }
+    }
+
+    public void stop() throws IOException {
+        if (connected) {
+            out.close();
+            sock.close();
+        }
+    }
+
 }
--- src/master/Network.java
+++ src/master/Network.java
@@ -3,7 +3,13 @@ package master;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
@@ -14,9 +20,8 @@ import javax.swing.SwingConstants;
 
 public class Network extends JPanel{
 
-    private ArrayList<String> llistaPotencials;
-    private ArrayList<String> llistaClients;
-    private int width;
+    private ArrayList<Emiter> llistaClients;
+    private final int width;
 
     public Network(Dimension screenSize) {
         super();
@@ -25,7 +30,6 @@ public class Network extends JPanel{
         setLocation(screenSize.width/20, 0);
         GroupLayout teclatLayout = new GroupLayout(this);
         setLayout(teclatLayout);
-        llistaPotencials = new ArrayList<>();
         llistaClients = new ArrayList<>();
         JLabel label = new JLabel("Client List");
         label.setSize(width, 25);
@@ -50,24 +54,37 @@ public class Network extends JPanel{
         add(boto);
     }
 
-    public void test() {
-        llistaPotencials.add("Pedrito");
-        llistaPotencials.add("Pablito");
-        llistaPotencials.add("Paquito");
-        llistaClients.add("Pablito");
-        populate();
+    public void test() throws IOException {
+        
+        int port = 42;
+        String locName = "Halfonso";
+        InetAddress add = Inet4Address.getByName("192.168.0.1");
+        InetSocketAddress inetSocketAddress = new InetSocketAddress(add, port);
+        Emiter e = new Emiter(inetSocketAddress, locName);
+        llistaClients.add(e);
+        Emiter e2 = new Emiter(inetSocketAddress, "Halfonso2");
+        llistaClients.add(e2);
+        Emiter e3 = new Emiter(inetSocketAddress, "Halfonso3");
+        llistaClients.add(e3);
+        e3.connect();
     }
     
     private void scan() {
-        test();
-        //omplir llista potencials
-        //checkboxes en consequencia
+        try {
+            test();
+        } catch (IOException ex) {
+            Logger.getLogger(Network.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        //omplir llista Emiters
+        populate();
+        repaint();
     }
 
     private void populate() {
         int height = 100;
-        for (String poten : llistaPotencials) {
-            JCheckBox cb = new JCheckBox(poten, llistaClients.contains(poten));
+        for (Emiter em : llistaClients) {
+            JCheckBox cb = new JCheckBox(em.getName(), em.isConnected());
             cb.setLocation(0, height);
             height += 25;
             cb.setFont(cb.getFont().deriveFont(25f));
@@ -78,7 +95,11 @@ public class Network extends JPanel{
                 public void actionPerformed(ActionEvent e) {
                     JCheckBox cb = ((JCheckBox) e.getSource());
                     if (cb.isSelected()) {
-                        llistaClients.add(cb.getName());
+                        for(Emiter em : llistaClients){
+                            if(em.amI(cb.getName())){
+                                em.connect();
+                            }
+                        }
                     }
                 }
             });
