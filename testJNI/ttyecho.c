b08f2ae24f49fe86d8785b714044ade25908a2a1
Josep Oriol Vilarrubi Barri <josep.vilarrubibarri@bsc.es>
2014-10-27 21:21:03 +0100

    added JNI stuff

--- testJNI/ttyecho.c
+++ testJNI/ttyecho.c
@@ -7,17 +7,14 @@
 #include <unistd.h>
 
 int main (int argc, char *argv[]) {
-    char *nl = "\n";
-	int fd = open(argv[1],O_RDWR);
+	int i,fd = open(argv[1],O_RDWR);
     if(fd == -1) {
         perror("open DEVICE");
         exit(1);
     }
-//	usleep(225000);
-    ioctl (fd, TIOCSTI, &(argv[2][0]));
-    ioctl (fd, TIOCSTI, &(argv[2][1]));
-    ioctl (fd, TIOCSTI, nl);
-//    ioctl (fd, TIOCSTI, 0);
+	for (i=0 ; i < argc; i++){
+	    ioctl (fd, TIOCSTI, &(argv[i][0]));
+	}
     close(fd);
     exit (0);
 }
